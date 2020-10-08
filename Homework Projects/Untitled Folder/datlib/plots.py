958ccaa34267de49160c97e2f2a6755b68cb1d91
tylerlenz <lenz.tyler9@gmail.com>
2020-10-07 21:17:38 -0500

    Updating python files

--- Homework Projects/Untitled Folder/datlib/plots.py
+++ Homework Projects/Untitled Folder/datlib/plots.py
@@ -35,3 +35,20 @@ def plot_ts_scatter(df, s = 75, figsize = (40, 20), save_fig = False, pp = None)
                 directory = "plots/" + x[:12] + " " + y[:12] + " c=Year"
                 plt.savefig(directory + ".png")
                 if pp != None: pp.savefig(fig, bbox_inches = "tight")
+                
+def plot_lines(df, linewidth = 1, figsize = (40,20), pp = None):
+    
+    fig, ax = plt.subplots(figsize = figsize)
+    df.plot.line(linewidth = linewidth, ax = ax)
+    ax.tick_params(axis = 'x', rotation = 90)
+    ax.tick_params('both', length = 0, which = 'both')
+    vals = ax.get_yticks()
+    ax.set_yticklabels([int(x) for x in vals])
+    
+    remove_chars = "[]:$'\\"
+    filename=str(list(df.keys()))
+    for char in remove_chars:
+        filename = filename.replace(char, "")
+    plt.savefig(filename[:50]+ " line.png", bbox_inches = "tight")
+    if pp != None:
+        pp.savefig(fig, bbox_inches = "tight")
